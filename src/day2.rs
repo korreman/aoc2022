abff6a41523b67a75241f9e2d7be990e02c454e4
Troels Korreman <troels.korreman@gmail.com>
2022-12-03 10:04:11 +0100

    Solve day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,2 +1,97 @@
+#[derive(Clone, Copy)]
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Clone, Copy)]
+enum Outcome {
+    Win,
+    Tie,
+    Lose,
+}
+
+impl Outcome {
+    fn score(self) -> u32 {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Tie => 3,
+            Outcome::Lose => 0,
+        }
+    }
+
+    fn from_letter(letter: char) -> Self {
+        match letter {
+            'X' => Outcome::Lose,
+            'Y' => Outcome::Tie,
+            'Z' => Outcome::Win,
+            _ => panic!(),
+        }
+    }
+}
+
+impl Play {
+    fn fight(self, other: Play) -> Outcome {
+        match (self, other) {
+            (Play::Rock, Play::Scissors) => Outcome::Win,
+            (Play::Rock, Play::Paper) => Outcome::Lose,
+            (Play::Paper, Play::Rock) => Outcome::Win,
+            (Play::Paper, Play::Scissors) => Outcome::Lose,
+            (Play::Scissors, Play::Paper) => Outcome::Win,
+            (Play::Scissors, Play::Rock) => Outcome::Lose,
+            _ => Outcome::Tie,
+        }
+    }
+
+    fn find_play(self, outcome: Outcome) -> Self {
+        match (self, outcome) {
+            (Play::Rock, Outcome::Win) => Play::Paper,
+            (Play::Rock, Outcome::Tie) => Play::Rock,
+            (Play::Rock, Outcome::Lose) => Play::Scissors,
+            (Play::Paper, Outcome::Win) => Play::Scissors,
+            (Play::Paper, Outcome::Tie) => Play::Paper,
+            (Play::Paper, Outcome::Lose) => Play::Rock,
+            (Play::Scissors, Outcome::Win) => Play::Rock,
+            (Play::Scissors, Outcome::Tie) => Play::Scissors,
+            (Play::Scissors, Outcome::Lose) => Play::Paper,
+        }
+    }
+
+    fn score(self) -> u32 {
+        match self {
+            Play::Rock => 1,
+            Play::Paper => 2,
+            Play::Scissors => 3,
+        }
+    }
+
+    fn from_letter(letter: char) -> Self {
+        match letter {
+            'A' => Play::Rock,
+            'B' => Play::Paper,
+            'C' => Play::Scissors,
+            'X' => Play::Rock,
+            'Y' => Play::Paper,
+            'Z' => Play::Scissors,
+            _ => panic!(),
+        }
+    }
+}
+
 pub fn run(input: &str) -> (u32, u32) {
+    let mut score1 = 0;
+    let mut score2 = 0;
+    for line in input.lines() {
+        let them = Play::from_letter(line.chars().nth(0).unwrap());
+        let me = Play::from_letter(line.chars().nth(2).unwrap());
+        score1 += me.score();
+        score1 += me.fight(them).score();
+
+        let desired_outcome = Outcome::from_letter(line.chars().nth(2).unwrap());
+        let actual_me = them.find_play(desired_outcome);
+        score2 += actual_me.score();
+        score2 += desired_outcome.score();
+    }
+    (score1, score2)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::time::Instant;
 
 mod day1;
+mod day2;
 
 fn main() {
     run(day1::run, "input/day1.txt", "Day 1");
+    run(day2::run, "input/day2.txt", "Day 2");
 }
 
 fn run<R: std::fmt::Display, T: Fn(&str) -> (R, R)>(task: T, input_path: &str, name: &str) {

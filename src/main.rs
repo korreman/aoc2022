78c721e2effb0c38005a1625cfcd6cec30ac36a2
Troels Korreman <troels.korreman@gmail.com>
2022-12-03 10:33:19 +0100

    Day 3 solution

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::time::Instant;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     run(day1::run, "input/day1.txt", "Day 1");
     run(day2::run, "input/day2.txt", "Day 2");
+    run(day3::run, "input/day3.txt", "Day 3");
 }
 
 fn run<R: std::fmt::Display, T: Fn(&str) -> (R, R)>(task: T, input_path: &str, name: &str) {

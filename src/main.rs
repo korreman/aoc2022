c4fcccd0a7887e55ce3486b2b96b5f86c22ac4c2
Troels Korreman <troels.korreman@gmail.com>
2022-12-03 19:12:06 +0100

    Pretty formatting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 use std::time::Instant;
 
+use comfy_table::{presets::UTF8_FULL_CONDENSED, Cell, CellAlignment, Table};
+
 mod day1;
 mod day2;
 mod day3;
 
 fn main() {
-    run(day1::run, "input/day1.txt", "Day 1");
-    run(day2::run, "input/day2.txt", "Day 2");
-    run(day3::run, "input/day3.txt", "Day 3");
-}
+    let mut table = Table::new();
+    table.load_preset(UTF8_FULL_CONDENSED);
+    table.set_header(vec!["Day", "Part 1", "Part 2", "Time"]);
 
-fn run<R: std::fmt::Display, T: Fn(&str) -> (R, R)>(task: T, input_path: &str, name: &str) {
-    let input = std::fs::read_to_string(input_path).unwrap();
+    let mut run_day = |day: u32, task: &dyn Fn(&str) -> (u32, u32), input_path: &str| {
+        let input = std::fs::read_to_string(input_path).unwrap();
 
-    let start = Instant::now();
-    let (res1, res2) = task(input.as_str());
-    let end = Instant::now();
+        let start = Instant::now();
+        let (res1, res2) = task(input.as_str());
+        let end = Instant::now();
+        let delta = end.duration_since(start);
 
-    let delta = end.duration_since(start);
-    println!(
-        "{} | Part 1: {} | Part 2: {} | Time: {:?}",
-        name,
-        res1,
-        res2,
-        delta
-    );
+        table.add_row(vec![
+            Cell::new(day.to_string()).set_alignment(CellAlignment::Right),
+            Cell::new(res1.to_string()).set_alignment(CellAlignment::Right),
+            Cell::new(res2.to_string()).set_alignment(CellAlignment::Right),
+            Cell::new(format!("{delta:.2?}")).set_alignment(CellAlignment::Right),
+        ]);
+    };
+
+    run_day(1, &day1::run, "input/day1.txt");
+    run_day(2, &day2::run, "input/day2.txt");
+    run_day(3, &day3::run, "input/day3.txt");
+
+    println!("{table}");
 }

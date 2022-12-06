a8f3e9092866cf9dea0a3ff5000d6c4164b2740f
Troels Korreman <troels.korreman@gmail.com>
2022-12-06 09:51:09 +0100

    Improve day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 pub fn run(input: &str) -> (usize, usize) {
     let res1 = task(input, 4);
     let res2 = task(input, 14);
@@ -7,19 +5,19 @@ pub fn run(input: &str) -> (usize, usize) {
 }
 
 fn task(input: &str, window_size: usize) -> usize {
-    let mut window: VecDeque<char> = input.chars().take(window_size - 1).collect();
-    let mut counter = window_size - 1;
-    for c in input.chars().skip(window_size - 1) {
-        counter += 1;
-        window.push_back(c);
-
-        if window
-            .iter()
-            .all(|a| window.iter().filter(|&b| b == a).count() == 1)
-        {
-            return counter;
-        }
-        window.pop_front().expect("popped empty window!");
-    }
-    panic!("no marker detected!")
+    input
+        .as_bytes()
+        .windows(window_size)
+        .enumerate()
+        .find_map(|(offset, bytes)| {
+            let p = bytes
+                .iter()
+                .all(|b| bytes.iter().filter(|b2| b == *b2).count() == 1);
+            if p {
+                Some(offset)
+            } else {
+                None
+            }
+        })
+        .expect("no packet marker detected!")
 }

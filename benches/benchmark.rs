de5ef0fbd7e0fb6023e9b6f82497c491c3bc05a0
Troels Korreman <troels.korreman@gmail.com>
2022-12-07 16:56:52 +0100

    Make benchmarks using a single macro

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,24 +1,39 @@
 use aoc2022::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+macro_rules! input {
+    ($day:ident) => {
+        let $day =
+            std::fs::read_to_string(concat!("input/", stringify!($day), ".txt")).unwrap();
+    };
+}
+
+macro_rules! run {
+    ($day:ident) => {
+        $day::run(black_box($day.as_str()))
+    };
+}
+
 macro_rules! bench {
     ( $c:ident, $day:ident) => {
         $c.bench_function(stringify!($day), |b| {
-            let input =
-                std::fs::read_to_string(concat!("input/", stringify!($day), ".txt")).unwrap();
-            b.iter(|| $day::run(black_box(input.as_str())))
+            b.iter(|| run!($day))
         });
     };
 }
 
+macro_rules! run_benchmarks {
+    ( $c:ident, $($day:ident),+ ) => {
+        $( input!($day); )+
+        $( bench!($c, $day); )+
+        $c.bench_function("all", |b| {b.iter(|| {
+            $( run!($day); )+
+        })});
+    };
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    bench!(c, day1);
-    bench!(c, day2);
-    bench!(c, day3);
-    bench!(c, day4);
-    bench!(c, day5);
-    bench!(c, day6);
-    bench!(c, day7);
+    run_benchmarks!(c, day1, day2, day3, day4, day5, day6, day7);
 }
 
 criterion_group!(benches, criterion_benchmark);

c94fa5f4983f1d7004c53223540b98497e7e9768
Troels Korreman <troels.korreman@gmail.com>
2022-12-07 18:08:10 +0100

    Day 4 speed improvement

--- src/day4.rs
+++ src/day4.rs
@@ -3,13 +3,22 @@ use itertools::Itertools;
 pub fn run(input: &str) -> (u32, u32) {
     let mut res1 = 0;
     let mut res2 = 0;
-    for line in input.lines() {
-        let ranges = line.split(['-', ',']).map(|n| n.parse::<u32>().unwrap());
+    for line in input
+        .as_bytes()
+        .split(|&b| b == b'\n')
+        .take_while(|l| l.len() > 0)
+    {
+        let ranges = line.split(|&b| b == b'-' || b == b',').map(|n| {
+            unsafe { std::str::from_utf8_unchecked(n) }
+                .parse::<u32>()
+                .unwrap()
+        });
         let (a, b, c, d) = ranges.collect_tuple().unwrap();
 
         if a <= c && d <= b || c <= a && b <= d {
             res1 += 1;
         }
+
         if a <= d && c <= b {
             res2 += 1;
         }

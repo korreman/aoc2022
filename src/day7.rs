df4d9aef061055c6aec0aabbfbc43ddbb4bf467a
Troels Korreman <troels.korreman@gmail.com>
2022-12-07 13:19:13 +0100

    Day 7 fixes

--- src/day7.rs
+++ src/day7.rs
@@ -3,9 +3,14 @@ use itertools::Itertools;
 
 #[derive(Debug)]
 struct Entry<'a> {
-    name: &'a str,
-    directory: bool,
-    size: Option<usize>,
+    size: u32,
+    name: Option<&'a str>,
+}
+
+impl<'a> Entry<'a> {
+    fn is_dir(&self) -> bool {
+        self.name.is_some()
+    }
 }
 
 struct DirTree<'a> {
@@ -17,14 +22,13 @@ impl<'a> DirTree<'a> {
     fn new() -> Self {
         let mut data = Arena::new();
         let root = data.new_node(Entry {
-            name: "/",
-            directory: true,
-            size: None,
+            size: 0,
+            name: Some("/"),
         });
         Self { arena: data, root }
     }
 
-    fn compute_dir_sizes(&mut self) -> usize {
+    fn compute_dir_sizes(&mut self) -> u32 {
         let mut total = 0;
         for edge in self
             .root
@@ -35,13 +39,13 @@ impl<'a> DirTree<'a> {
             match edge {
                 indextree::NodeEdge::End(id) => {
                     let node = self.arena.get(*id).unwrap().get();
-                    let size = node.size.unwrap();
-                    if size <= 100000 && node.directory {
+                    let size = node.size;
+                    if size <= 100000 && node.is_dir() {
                         total += size;
                     }
                     if let Some(parent) = id.ancestors(&self.arena).skip(1).next() {
                         let parent = self.arena.get_mut(parent).unwrap().get_mut();
-                        *parent.size.get_or_insert(0) += size;
+                        parent.size += size;
                     }
                 }
                 _ => (),
@@ -51,7 +55,7 @@ impl<'a> DirTree<'a> {
     }
 }
 
-pub fn run(input: &str) -> (usize, usize) {
+pub fn run(input: &str) -> (u32, u32) {
     let mut tree = DirTree::new();
     let mut dir = tree.root;
     let mut lines = input.lines().peekable();
@@ -61,28 +65,25 @@ pub fn run(input: &str) -> (usize, usize) {
                 match listing.split_once(' ').unwrap() {
                     ("dir", name) => {
                         let node = tree.arena.new_node(Entry {
-                            name: name,
-                            directory: true,
-                            size: None,
+                            size: 0,
+                            name: Some(name),
                         });
                         dir.append(node, &mut tree.arena);
                     }
-                    (size, name) => {
-                        let size = size.parse::<usize>().unwrap();
-                        let node = tree.arena.new_node(Entry {
-                            name: name,
-                            directory: false,
-                            size: Some(size),
-                        });
+                    (size, _) => {
+                        let size = size.parse::<u32>().unwrap();
+                        let node = tree.arena.new_node(Entry { size, name: None });
                         dir.append(node, &mut tree.arena);
                     }
                 }
             }
+        } else if line == "$ cd /" {
+            dir = tree.root;
         } else if line.starts_with("$ cd ..") {
             dir = tree.arena[dir].parent().unwrap();
         } else if let Some(dest) = line.strip_prefix("$ cd ") {
             for child in dir.children(&tree.arena) {
-                if tree.arena[child].get().name == dest {
+                if tree.arena[child].get().name == Some(dest) {
                     dir = child;
                 }
             }
@@ -93,13 +94,13 @@ pub fn run(input: &str) -> (usize, usize) {
 
     let res1 = tree.compute_dir_sizes();
 
-    let free_space = 70_000_000 - tree.arena[tree.root].get().size.unwrap();
+    let free_space = 70_000_000 - tree.arena[tree.root].get().size;
     let space_to_free = 30_000_000 - free_space;
     let res2 = tree
         .arena
         .iter()
         .filter_map(|node| {
-            let size = node.get().size.unwrap();
+            let size = node.get().size;
             if size > space_to_free {
                 Some(size)
             } else {

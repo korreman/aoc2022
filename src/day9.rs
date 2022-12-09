f200baeaecc308f0d16afcf5d6d4d215b66a53af
Troels Korreman <troels.korreman@gmail.com>
2022-12-09 10:59:12 +0100

    Use a vec rather than a hashset

--- src/day9.rs
+++ src/day9.rs
@@ -1,21 +1,20 @@
 use ascii::{AsciiChar, AsciiStr};
-use std::{collections::HashSet};
 
 pub fn run(input: &AsciiStr) -> (usize, usize) {
-    let (mut rope1, mut rope2) = ([(0, 0); 2], [(0,0); 10]);
-    let (mut trail1, mut trail2) = (HashSet::from([(0, 0)]), HashSet::from([(0, 0)]));
+    let (mut rope1, mut rope2) = ([(0, 0); 2], [(0, 0); 10]);
+    let (mut trail1, mut trail2) = (vec![(0, 0)], vec![(0, 0)]);
     for line in input.trim_end().split(AsciiChar::LineFeed) {
         let steps = line[2..].as_str().parse::<u32>().unwrap();
         for _ in 0..steps {
             if step_rope(&mut rope1, line[0]) {
-                trail1.insert(*rope1.last().unwrap());
+                trail1.push(*rope1.last().unwrap());
             }
             if step_rope(&mut rope2, line[0]) {
-                trail2.insert(*rope2.last().unwrap());
+                trail2.push(*rope2.last().unwrap());
             }
         }
     }
-    (trail1.len(), trail2.len())
+    (count_unique(trail1), count_unique(trail2))
 }
 
 fn step_rope(rope: &mut [(i32, i32)], dir: AsciiChar) -> bool {
@@ -39,6 +38,18 @@ fn step_rope(rope: &mut [(i32, i32)], dir: AsciiChar) -> bool {
         }
     }
     return true;
+}
+
+fn count_unique<T: Copy + Default + Ord>(mut data: Vec<T>) -> usize {
+    data.sort_unstable();
+    let (res, _) = data.iter().fold((0, T::default()), |(count, x), y| {
+        if x == *y {
+            (count, x)
+        } else {
+            (count + 1, *y)
+        }
+    });
+    res
 }
 
 #[cfg(test)]

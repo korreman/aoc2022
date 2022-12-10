349a96148af2f5714bddcb527fd20a274153a435
Troels Korreman <troels.korreman@gmail.com>
2022-12-10 23:01:24 +0100

    Day 10 can't stop wont stop

--- src/day10.rs
+++ src/day10.rs
@@ -1,13 +1,12 @@
 use ascii::AsciiStr;
 use std::fmt::{Debug, Display, Write};
 
+#[inline]
 pub fn run(input: &AsciiStr) -> (i32, Res2) {
     let input = input.as_bytes();
-    let mut clock: u16 = 0;
-    let mut reg_x: i32 = 1;
-    let (mut res1, mut res2) = (0i32, Res2::new());
-    let mut idx = 0;
 
+    let (mut res1, mut res2) = (0i32, Res2::new());
+    let mut clock: u16 = 0;
     let mut tick = |x: i32| {
         if x.abs_diff(clock as i32 % 40) <= 1 {
             res2.blit(clock);
@@ -19,20 +18,22 @@ pub fn run(input: &AsciiStr) -> (i32, Res2) {
         clock < 240
     };
 
+    let mut reg_x: i32 = 1;
+    let mut idx = 0;
     while idx < input.len() && tick(reg_x) {
         // Virtual machine
         if input[idx] == b'a' {
             tick(reg_x);
-            // 1. Jump to number.
+            // Advance to number
             idx += 5;
-            // 2. Check for '-' sign.
+            // Check for sign
             let sign: i32 = if input[idx] == b'-' {
                 idx += 1;
                 -1
             } else {
                 1
             };
-            // 3. Parse number into v.
+            // Parse number
             let mut v = 0i32;
             while input[idx] != b'\n' {
                 v *= 10;
@@ -40,9 +41,8 @@ pub fn run(input: &AsciiStr) -> (i32, Res2) {
                 idx += 1;
             }
             idx += 1;
-            v *= sign;
-            // 4. Add or subtract v from reg_x.
-            reg_x += v;
+            // 4
+            reg_x += sign * v;
         } else {
             idx += 5;
         }
@@ -50,36 +50,21 @@ pub fn run(input: &AsciiStr) -> (i32, Res2) {
     (res1, res2)
 }
 
-//pub fn run_simple(input: &AsciiStr) -> (i32, Res2) {
-//    let mut reg_x = 1i32;
-//    let (mut res1, mut res2) = (0i32, [false; 240]);
-//    for (clock, element) in input.as_str().split_ascii_whitespace().enumerate() {
-//        // Result gathering
-//        if reg_x.abs_diff(clock as i32 % 40) <= 1 {
-//            res2[clock] = true;
-//        }
-//        if (clock + 1) % 40 == 20 {
-//            res1 += reg_x * (clock + 1) as i32;
-//        }
-//        reg_x += element.parse::<i32>().unwrap_or(0);
-//    }
-//    (res1, Res2 { data: res2 })
-//}
-
 pub struct Res2 {
     data: [u32; 8],
 }
 
 impl Res2 {
+    const BITSHIFT: u32 = 5;
+    const MASK: u16 = 0x1F;
+
     fn new() -> Self {
-        Self {
-            data: [0; 8],
-        }
+        Self { data: [0; 8] }
     }
 
     #[inline]
     fn blit(&mut self, idx: u16) {
-        self.data[(idx >> 5) as usize] |= 1 << (idx & 0x1F);
+        self.data[(idx >> Self::BITSHIFT) as usize] |= 1 << (idx & Self::MASK);
     }
 }
 
@@ -92,10 +77,11 @@ impl Display for Res2 {
 impl Debug for Res2 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("Day 10, part 2:\n")?;
-        for y in 0..6 {
-            for x in 0..40 {
+        for y in 0..6 as u16 {
+            for x in 0..40 as u16 {
                 let idx = x + y * 40;
-                let bit = self.data[idx >> 5] & (1 << (idx & 0x1F)) != 0;
+                let bit =
+                    self.data[(idx >> Self::BITSHIFT) as usize] & (1 << (idx & Self::MASK)) != 0;
                 let c = if bit { '█' } else { '░' };
                 f.write_char(c)?;
             }

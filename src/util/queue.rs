b222a8ea8a8ca71ab5c3f60f5489d3c2941e59ab
Troels Korreman <troels.korreman@gmail.com>
2022-12-12 20:26:22 +0100

    Day 8, functioning A\* solution

--- src/util/queue.rs
+++ src/util/queue.rs
@@ -1,14 +1,18 @@
-use std::collections::VecDeque;
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
 
-pub trait PriorityQueue<P: PartialOrd, T> {
+pub trait PriorityQueue<P, T> {
     fn new() -> Self;
     fn add(&mut self, priority: P, item: T);
     fn next(&mut self) -> Option<(P, T)>;
+    fn len(&self) -> usize;
 }
 
 pub struct SlidingBucketQueue<const R: usize, T> {
     offset: usize,
-    items: usize,
+    count: usize,
     pub queue: VecDeque<Vec<T>>,
 }
 
@@ -16,7 +20,7 @@ impl<const R: usize, T: Clone> PriorityQueue<usize, T> for SlidingBucketQueue<R,
     fn new() -> Self {
         Self {
             offset: 0,
-            items: 0,
+            count: 0,
             queue: VecDeque::from(vec![Vec::new(); R]),
         }
     }
@@ -24,14 +28,14 @@ impl<const R: usize, T: Clone> PriorityQueue<usize, T> for SlidingBucketQueue<R,
     fn add(&mut self, priority: usize, item: T) {
         if (self.offset..self.offset + R).contains(&priority) {
             self.queue[priority - self.offset].push(item);
-            self.items += 1;
+            self.count += 1;
         } else {
             panic!("priority {priority} outside current range");
         }
     }
 
     fn next(&mut self) -> Option<(usize, T)> {
-        if self.items == 0 {
+        if self.count == 0 {
             return None;
         }
         loop {
@@ -42,5 +46,62 @@ impl<const R: usize, T: Clone> PriorityQueue<usize, T> for SlidingBucketQueue<R,
                 self.queue.rotate_left(1);
             }
         }
+    }
+
+    fn len(&self) -> usize {
+        self.count
+    }
+}
+
+impl<T> PriorityQueue<usize, T> for BinaryHeap<Reverse<KVPair<usize, T>>> {
+    fn new() -> Self {
+        BinaryHeap::new()
+    }
+
+    fn add(&mut self, priority: usize, item: T) {
+        self.push(Reverse(KVPair::new(priority, item)));
+    }
+
+    fn next(&mut self) -> Option<(usize, T)> {
+        self.pop().map(|entry| KVPair::extract(entry.0))
+    }
+
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+pub struct KVPair<K, V> {
+    key: K,
+    value: V,
+}
+
+impl<K, V> KVPair<K, V> {
+    fn new(key: K, value: V) -> Self {
+        Self { key, value }
+    }
+
+    fn extract(self) -> (K, V) {
+        (self.key, self.value)
+    }
+}
+
+impl<K: PartialEq, V> PartialEq for KVPair<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.key.eq(&other.key)
+    }
+}
+
+impl<K: PartialOrd, V> PartialOrd for KVPair<K, V> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.key.partial_cmp(&other.key)
+    }
+}
+
+impl<K: Eq, V> Eq for KVPair<K, V> {}
+
+impl<K: Ord, V> Ord for KVPair<K, V> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.key.cmp(&other.key)
     }
 }

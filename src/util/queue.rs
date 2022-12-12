b9d8c2e59710327797ff3e2766ea7a1fbf381fde
Troels Korreman <troels.korreman@gmail.com>
2022-12-12 18:05:51 +0100

    Abstract priority queue

--- src/util/queue.rs
+++ src/util/queue.rs
@@ -1,41 +1,46 @@
 use std::collections::VecDeque;
 
-#[derive(Clone)]
-pub struct SlidingBucketQueue<T> {
+pub trait PriorityQueue<P: PartialOrd, T> {
+    fn new() -> Self;
+    fn add(&mut self, priority: P, item: T);
+    fn next(&mut self) -> Option<(P, T)>;
+}
+
+pub struct SlidingBucketQueue<const R: usize, T> {
     offset: usize,
+    items: usize,
     pub queue: VecDeque<Vec<T>>,
 }
 
-impl<T: Clone> SlidingBucketQueue<T> {
-    pub fn new(range: usize) -> Self {
+impl<const R: usize, T: Clone> PriorityQueue<usize, T> for SlidingBucketQueue<R, T> {
+    fn new() -> Self {
         Self {
             offset: 0,
-            queue: VecDeque::from(vec![Vec::new(); range]),
+            items: 0,
+            queue: VecDeque::from(vec![Vec::new(); R]),
         }
     }
 
-    pub fn add(&mut self, priority: usize, value: T) -> bool {
-        if let Some(bucket) = self.queue.get_mut(priority - self.offset) {
-            bucket.push(value);
-            true
+    fn add(&mut self, priority: usize, item: T) {
+        if (self.offset..self.offset + R).contains(&priority) {
+            self.queue[priority - self.offset].push(item);
+            self.items += 1;
         } else {
-            false
+            panic!("priority {priority} outside current range");
         }
     }
 
-    pub fn next(&mut self) -> Option<(usize, T)> {
-        let mut total_rotation = 0;
-        let value = loop {
+    fn next(&mut self) -> Option<(usize, T)> {
+        if self.items == 0 {
+            return None;
+        }
+        loop {
             if let Some(v) = self.queue[0].pop() {
-                break v;
-            } else if total_rotation < self.queue.len() {
-                total_rotation += 1;
+                return Some((self.offset, v));
+            } else {
                 self.offset += 1;
                 self.queue.rotate_left(1);
-            } else {
-                return None;
             }
-        };
-        Some((self.offset, value))
+        }
     }
 }

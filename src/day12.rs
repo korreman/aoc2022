9df27df01228195a8982fe751993b6b5d30405ab
Troels Korreman <troels.korreman@gmail.com>
2022-12-12 23:39:03 +0100

    Add radix heap solution

--- src/day12.rs
+++ src/day12.rs
@@ -3,7 +3,7 @@ use std::{cmp::Reverse, collections::BinaryHeap};
 use crate::util::{
     grid::{Grid, Pos},
     pathfinding::{a_star, dijkstra},
-    queue::{KVPair, SlidingBucketQueue},
+    queue::{KVPair, RadixHeap, SlidingBucketQueue},
 };
 use ascii::AsciiStr;
 
@@ -18,37 +18,19 @@ pub fn run(input: &AsciiStr) -> (usize, usize) {
         c => c as u8 - b'a',
     });
 
-    type Queue1 = BinaryHeap<Reverse<KVPair<usize, Pos>>>;
-    let res1 = a_star::<Queue1, _, _, _, _>(
-        &grid,
-        start,
-        |a, b| {
-            if grid[b] as i16 - grid[a] as i16 <= 1 {
-                Some(1)
-            } else {
-                None
-            }
-        },
-        |p| p.dist(&end),
-        |pos| pos == end,
-    )
-    .unwrap_or(0);
+    type Queue = RadixHeap<Pos>;
+    let cost = |a, b| {
+        if grid[a] as i16 - grid[b] as i16 <= 1 {
+            Some(1)
+        } else {
+            None
+        }
+    };
+    let res1 = a_star::<Queue, _, _, _, _>(&grid, end, cost, |p| p.dist(&end), |pos| pos == start);
 
-    let res2 = dijkstra::<SlidingBucketQueue<2, Pos>, _, _, _>(
-        &grid,
-        end,
-        |a, b| {
-            if grid[a] as i16 - grid[b] as i16 <= 1 {
-                Some(1)
-            } else {
-                None
-            }
-        },
-        |p| grid[p] == 0,
-    )
-    .unwrap();
+    let res2 = a_star::<Queue, _, _, _, _>(&grid, end, cost, |p| p.x, |p| grid[p] == 0);
 
-    (res1, res2)
+    (res1.unwrap(), res2.unwrap())
 }
 
 #[cfg(test)]

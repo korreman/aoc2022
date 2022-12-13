a42c6b426f0d6c9197a2d067a665b983865a16c2
Troels Korreman <troels.korreman@gmail.com>
2022-12-13 22:19:30 +0100

    Fast day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,5 @@
 use ascii::AsciiStr;
+use std::mem::swap;
 
 pub fn run(input: &AsciiStr) -> (u32, u32) {
     let mut elf_calories = 0;
@@ -18,4 +19,33 @@ pub fn run(input: &AsciiStr) -> (u32, u32) {
     let b = elfs.pop().unwrap() + elfs.pop().unwrap() + elfs.pop().unwrap();
 
     (a, b)
+}
+
+pub fn run_fast(input: &AsciiStr) -> (u32, u32) {
+    //let mut maxes = [0u32; 3];
+    let mut max = 0;
+
+    let mut acc = 0;
+    let mut calories = 0;
+    for b in input.as_bytes() {
+        if *b != b'\n' {
+            acc *= 10;
+            acc += (b - b'0') as u32;
+        } else {
+            if acc > 0 {
+                calories += acc;
+                acc = 0;
+            } else {
+                max = max.max(calories);
+                //for max in &mut maxes {
+                //    if *max < calories {
+                //        swap(max, &mut calories);
+                //    }
+                //}
+                calories = 0;
+            }
+        }
+    }
+    (max, 0)
+    //(maxes[0], maxes[0] + maxes[1] + maxes[2])
 }

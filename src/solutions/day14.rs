0cfb9b2955aed7137e58349b93ecd6cd68434662
Troels Korreman <troels.korreman@gmail.com>
2022-12-14 10:06:35 +0100

    Solve day 14

---
+++ src/solutions/day14.rs
@@ -0,0 +1,113 @@
+use std::fmt::{Display, Write};
+
+use crate::util::grid::{Grid, Pos};
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Cell {
+    Air,
+    Rock,
+    Sand,
+}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Cell::Air => '.',
+            Cell::Rock => '#',
+            Cell::Sand => 'o',
+        };
+        f.write_char(c)
+    }
+}
+
+pub fn run(input: &str) -> (usize, usize) {
+    // Parse
+    let structures: Vec<Vec<Pos>> = input
+        .lines()
+        .map(|line| {
+            line.split(" -> ")
+                .map(|elem| {
+                    let (x, y) = elem.split_once(',').unwrap();
+                    let x = x.parse().unwrap();
+                    let y = y.parse().unwrap();
+                    Pos { x, y }
+                })
+                .collect()
+        })
+        .collect();
+    let spawn = 500;
+
+    let width = 1000;
+    let height = structures.iter().flatten().map(|p| p.y).max().unwrap() + 2;
+
+    let mut grid = Grid::new_filled(width, height, Cell::Air);
+    for structure in &structures {
+        for (a, b) in structure.iter().tuple_windows() {
+            let range = if a.x != b.x {
+                (usize::min(a.x, b.x)..=usize::max(a.x, b.x))
+                    .map(|x| Pos { x, y: a.y })
+                    .collect_vec()
+            } else {
+                (usize::min(a.y, b.y)..=usize::max(a.y, b.y))
+                    .map(|y| Pos { x: a.x, y })
+                    .collect_vec()
+            };
+
+            for pos in range {
+                grid[pos] = Cell::Rock;
+            }
+        }
+    }
+
+    // Simulate
+    let mut res1 = 0;
+    let mut res2 = 0;
+    'simulation: loop {
+        let mut p = Pos { x: spawn, y: 0 };
+        'falling: loop {
+            for &target in &[
+                Pos { x: p.x, y: p.y + 1 },
+                Pos {
+                    x: p.x - 1,
+                    y: p.y + 1,
+                },
+                Pos {
+                    x: p.x + 1,
+                    y: p.y + 1,
+                },
+            ] {
+                if p.y == grid.height() - 1 {
+                    if res1 == 0 {
+                        res1 = res2;
+                    }
+                    break 'falling;
+                }
+                if grid[target] == Cell::Air {
+                    p = target;
+                    continue 'falling;
+                }
+            }
+            if p == (Pos { x: spawn, y: 0 }) {
+                break 'simulation;
+            }
+            break 'falling;
+        }
+        let c = grid.get_mut(p);
+        assert!(c == Some(&mut Cell::Air), "Cell {p:?} isn't air!\n{grid}!");
+        grid[p] = Cell::Sand;
+        res2 += 1;
+    }
+
+    (res1, res2 + 1)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {
+        let input = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9";
+        let (res1, res2) = super::run(input);
+        assert_eq!(res1, 24);
+    }
+}

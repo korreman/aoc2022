fa7403bf3c300d4d326166891089e0913fbe1125
Troels Korreman <troels.korreman@gmail.com>
2022-12-17 14:29:10 +0100

    Day 11, convert to bitvec solution

--- src/solutions/day17.rs
+++ src/solutions/day17.rs
@@ -1,58 +1,83 @@
-use std::fmt::{Display, Write};
+use itertools::Itertools;
+use std::{
+    collections::VecDeque,
+    fmt::{Display, Write},
+};
 
-use crate::util::grid::{pos, Pos};
+// Rows are 1-byte bit vectors.
+// Rightmost cell is lowest bit.
+// Highest bit is unused.
+type Row = u8;
+// Shapes are 4 row bytes stored in a u32.
+// Lowest row is lowest byte.
+type Row4 = u32;
 
-type Shape = Vec<Pos>;
+//
+const SHAPES: [Row4; 5] = [
+    0b00011110,                            // horizontal line
+    0b00001000_00011100_00001000,          // cross
+    0b00000100_00000100_00011100,          // flipped L
+    0b00010000_00010000_00010000_00010000, // vertical line
+    0b00011000_00011000,                   // box
+];
 
-struct Board {
-    grid: Vec<[bool; 9]>,
+const WALL_LEFT: Row4 = 0b01000000_01000000_01000000_01000000;
+const WALL_RIGHT: Row4 = 0b00000001_00000001_00000001_00000001;
+
+struct Tower {
+    /// Number of rows that have been freed.
+    forgotten: usize,
+    rows: VecDeque<Row>,
 }
 
-impl Board {
+impl Tower {
     fn new() -> Self {
         Self {
-            grid: vec![[true; 9]],
+            forgotten: 0,
+            rows: VecDeque::from(vec![0b01111111, 0, 0, 0]),
         }
     }
 
-    fn next_pos(&self) -> Pos {
-        pos(3, self.grid.len() + 3)
-    }
-
-    fn test_pos(&self, p: Pos) -> bool {
-        if !(1..=7).contains(&p.x) {
-            false
-        } else if self.grid.len() <= p.y {
-            true
-        } else {
-            !self.grid[p.y][p.x]
+    fn place(&mut self, mut shape: Row4, wind: &mut impl Iterator<Item = u8>) {
+        let mut window: Row4 = 0;
+        // Ensure a buffer of 4 empty rows above the highest rock
+        while self.rows[self.rows.len() - 4] != 0 {
+            self.rows.push_back(0);
         }
-    }
-
-    fn test_shape(&self, p: Pos, shape: &Shape) -> bool {
-        shape.iter().all(|sp| {
-            let point = pos(p.x + sp.x, p.y + sp.y);
-            self.test_pos(point)
-        })
-    }
-
-    const EMPTY_ROW: [bool; 9] = [true, false, false, false, false, false, false, false, true];
-    fn add_shape(&mut self, p: Pos, shape: &Shape) {
-        for sp in shape {
-            let point = pos(p.x + sp.x, p.y + sp.y);
-            while point.y >= self.grid.len() {
-                self.grid.push(Self::EMPTY_ROW);
+        for (row_idx, row_data) in self.rows.iter().cloned().enumerate().rev().skip(1) {
+            // Attempt to move the shape
+            let blown_shape = match wind.next().unwrap() {
+                b'<' if shape & WALL_LEFT == 0 => shape << 1,
+                b'>' if shape & WALL_RIGHT == 0 => shape >> 1,
+                _ => shape,
+            };
+            // Don't move if blocked by rocks
+            if blown_shape & window == 0 {
+                shape = blown_shape;
             }
-            self.grid[point.y][point.x] = true;
+            // Move window down one row
+            window = (window << 8) | row_data as u32;
+            // If the shape now collides, we must place it one row above
+            if shape & window != 0 {
+                for (i, b) in shape.to_le_bytes().iter().enumerate() {
+                    self.rows[(row_idx + 1) + i] |= *b;
+                }
+                break;
+            }
         }
+    }
+
+    fn height(&self) -> usize {
+        self.forgotten + self.rows.iter().enumerate().rev().find(|x| *x.1 != 0).unwrap().0
     }
 }
 
-impl Display for Board {
+impl Display for Tower {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.grid.iter().rev() {
-            for cell in row {
-                if *cell {
+        for row in self.rows.iter().rev() {
+            for i in (0..8).rev() {
+                let cell = row & (1 << i) != 0;
+                if cell {
                     f.write_char('#')?;
                 } else {
                     f.write_char('.')?;
@@ -65,40 +90,17 @@ impl Display for Board {
 }
 
 pub fn run(input: &str) -> (usize, usize) {
-    let shapes: [Shape; 5] = [
-        vec![pos(0, 0), pos(1, 0), pos(2, 0), pos(3, 0)],
-        vec![pos(1, 0), pos(0, 1), pos(1, 1), pos(2, 1), pos(1, 2)],
-        vec![pos(0, 0), pos(1, 0), pos(2, 0), pos(2, 1), pos(2, 2)],
-        vec![pos(0, 0), pos(0, 1), pos(0, 2), pos(0, 3)],
-        vec![pos(0, 0), pos(1, 0), pos(0, 1), pos(1, 1)],
-    ];
     let mut wind = input.trim().bytes().cycle();
-    let mut shapes = shapes.iter().cycle();
+    let mut shapes = SHAPES.iter().cycle();
+
+    let mut board = Tower::new();
     let mut shape = shapes.next().unwrap();
-    let mut board = Board::new();
-    let mut p = board.next_pos();
-    for i in 0..2022 {
-        loop {
-            let wind_p = match wind.next().unwrap() {
-                b'>' => pos(p.x + 1, p.y),
-                b'<' => pos(p.x - 1, p.y),
-                _ => panic!(),
-            };
-            if board.test_shape(wind_p, shape) {
-                p = wind_p;
-            }
-            let fall_p = pos(p.x, p.y - 1);
-            if board.test_shape(fall_p, shape) {
-                p = fall_p;
-            } else {
-                board.add_shape(p, shape);
-                shape = shapes.next().unwrap();
-                p = board.next_pos();
-                break;
-            }
-        }
+    for _ in 0..2022 {
+        //println!("{board}\n");
+        board.place(*shape, &mut wind);
+        shape = shapes.next().unwrap();
     }
-    (board.grid.len() - 1, 0)
+    (board.height(), 0)
 }
 
 #[cfg(test)]

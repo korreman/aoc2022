c537ef43b06fc0c9eccd85d9a1321a025d53c908
Troels Korreman <troels.korreman@gmail.com>
2022-12-27 19:27:07 +0100

    Cleanup

--- src/util/dfs.rs
+++ src/util/dfs.rs
@@ -15,22 +15,13 @@ pub trait Dfs {
         let mut history: Vec<Self::Trace> = Vec::new();
         let mut stack: Vec<Self::Actions> = vec![self.actions()];
         let mut best_score = self.score();
-        let mut counter = 0u64;
         while let Some(actions) = stack.last_mut() {
-            counter += 1;
-            if counter % 10000000 == 0 {
-                println!("{:?}/{:?} ||| {history:?}", self.score(), best_score);
-            }
             if let Some(action) = actions.next() {
                 if let Some(trace) = self.perform(&history, &action) {
                     history.push(trace);
                     stack.push(self.actions());
                 }
             } else {
-                //if self.score() > best_score {
-                //    println!("{best_score:?}");
-                //    println!("{history:?}");
-                //}
                 best_score = best_score.max(self.score());
                 if let Some(trace) = history.pop() {
                     self.backtrack(&history, trace);

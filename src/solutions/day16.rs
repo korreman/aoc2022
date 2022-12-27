20141c364fee75715e22953a5641500c65aff19b
Troels Korreman <troels.korreman@gmail.com>
2022-12-27 19:21:08 +0100

    Solve day 16, part 2

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -16,7 +16,7 @@ fn parse_valve<'a>(line: &str) -> Option<(&str, std::vec::IntoIter<&str>, u64)> 
     Some((label, tunnels.into_iter(), flow))
 }
 
-struct DfsState<'a> {
+struct State1<'a> {
     valves: &'a Vec<u64>,
     costs: &'a Grid<u64>,
 
@@ -26,7 +26,7 @@ struct DfsState<'a> {
     score: u64,
 }
 
-impl<'a> DfsState<'a> {
+impl<'a> State1<'a> {
     fn new(valves: &'a Vec<u64>, costs: &'a Grid<u64>, node: usize) -> Self {
         Self {
             valves,
@@ -47,9 +47,10 @@ impl<'a> DfsState<'a> {
     }
 }
 
-impl<'a> Dfs for DfsState<'a> {
+impl<'a> Dfs for State1<'a> {
     type Action = usize;
     type Actions = Range<usize>;
+    type Trace = usize;
     type Score = u64;
 
     fn actions(&self) -> Self::Actions {
@@ -60,27 +61,115 @@ impl<'a> Dfs for DfsState<'a> {
         self.score
     }
 
-    fn perform(&mut self, history: &Vec<usize>, action: &usize) -> bool {
-        // The cost of moving to a node and then opening it.
+    fn perform(&mut self, history: &Vec<usize>, action: &usize) -> Option<usize> {
         let cost = self.cost(*action);
         if self.steps_left >= cost && !history.contains(action) {
             self.steps_left -= cost;
             self.node = *action;
             self.score += self.score_increase();
-            true
+            Some(*action)
         } else {
-            false
+            None
         }
     }
 
-    fn backtrack(&mut self, history: &Vec<usize>, action: &Self::Action) {
+    fn backtrack(&mut self, history: &Vec<usize>, trace: usize) {
         self.score -= self.score_increase();
         self.node = *history.last().unwrap_or(&self.start);
-        self.steps_left += self.cost(*action);
+        self.steps_left += self.cost(trace);
     }
 }
 
-pub fn run(input: &str) -> (u64, usize) {
+struct State2<'a> {
+    valves: &'a Vec<u64>,
+    costs: &'a Grid<u64>,
+
+    actors: [Actor; 2],
+    score: u64,
+}
+
+struct Actor {
+    node: usize,
+    steps_left: u64,
+}
+
+impl<'a> State2<'a> {
+    fn new(valves: &'a Vec<u64>, costs: &'a Grid<u64>, node: usize) -> Self {
+        Self {
+            valves,
+            costs,
+            actors: [
+                Actor {
+                    node,
+                    steps_left: 26,
+                },
+                Actor {
+                    node,
+                    steps_left: 26,
+                },
+            ],
+            score: 0,
+        }
+    }
+
+    fn score_increase(&self, node: usize, steps_left: u64) -> u64 {
+        self.valves[node] * steps_left
+    }
+
+    fn cost(&self, from: usize, to: usize) -> u64 {
+        self.costs[pos(from, to)] + 1
+    }
+
+    fn lowest_actor(&self) -> usize {
+        if self.actors[0].steps_left > self.actors[1].steps_left {
+            0
+        } else {
+            1
+        }
+    }
+}
+
+impl<'a> Dfs for State2<'a> {
+    type Score = u64;
+    type Action = usize;
+    type Actions = Range<usize>;
+    type Trace = (usize, usize);
+
+    fn score(&self) -> Self::Score {
+        self.score
+    }
+
+    fn actions(&self) -> Self::Actions {
+        0..self.valves.len()
+    }
+
+    fn perform(
+        &mut self,
+        history: &Vec<Self::Trace>,
+        action: &Self::Action,
+    ) -> Option<Self::Trace> {
+        let i = self.lowest_actor();
+        let cost: u64 = self.cost(self.actors[i].node, *action);
+        if self.actors[i].steps_left >= cost && history.iter().all(|(_, dst)| dst != action) {
+            self.actors[i].steps_left -= cost;
+            let prev = self.actors[i].node;
+            self.actors[i].node = *action;
+            self.score += self.score_increase(self.actors[i].node, self.actors[i].steps_left);
+            Some((prev, *action))
+        } else {
+            None
+        }
+    }
+
+    fn backtrack(&mut self, _history: &Vec<Self::Trace>, trace: Self::Trace) {
+        let i = if self.actors[0].node == trace.1 { 0 } else { 1 };
+        self.score -= self.score_increase(self.actors[i].node, self.actors[i].steps_left);
+        self.actors[i].node = trace.0;
+        self.actors[i].steps_left += self.cost(self.actors[i].node, trace.1);
+    }
+}
+
+pub fn run(input: &str) -> (u64, u64) {
     // Parse into a graph
     let mut valves: HashGraph<&str, u64> = input.lines().map(|l| parse_valve(l).unwrap()).collect();
 
@@ -119,11 +208,13 @@ pub fn run(input: &str) -> (u64, usize) {
         .position(|&x| x == valves.start)
         .unwrap();
 
-    let mut dfs_state = DfsState::new(&new_valves, &cost_grid, dfs_start);
     println!("{cost_grid:3}");
-    println!("{}, {:?}", dfs_start, new_valves,);
-    let res1 = dfs_state.dfs();
-    (res1, 0)
+    println!("{}, {:?}", dfs_start, new_valves);
+    let mut dfs_state1 = State1::new(&new_valves, &cost_grid, dfs_start);
+    let mut dfs_state2 = State2::new(&new_valves, &cost_grid, dfs_start);
+    let res1 = dfs_state1.dfs();
+    let res2 = dfs_state2.dfs();
+    (res1, res2)
 }
 
 #[cfg(test)]
@@ -141,6 +232,6 @@ mod tests {
             Valve HH has flow rate=22; tunnel leads to valve GG\n\
             Valve II has flow rate=0; tunnels lead to valves AA, JJ\n\
             Valve JJ has flow rate=21; tunnel leads to valve II";
-        assert_eq!(super::run(input).0, 1651);
+        assert_eq!(super::run(input), (1651, 1707));
     }
 }
--- src/util/dfs.rs
+++ src/util/dfs.rs
@@ -1,28 +1,39 @@
 use std::fmt::Debug;
 
 pub trait Dfs {
-    type Score: Ord;
+    type Score: Ord + Debug;
     type Action: Clone + Debug;
     type Actions: Iterator<Item = Self::Action>;
+    type Trace: Debug;
     fn score(&self) -> Self::Score;
     fn actions(&self) -> Self::Actions;
-    fn perform(&mut self, history: &Vec<Self::Action>, action: &Self::Action) -> bool;
-    fn backtrack(&mut self, history: &Vec<Self::Action>, action: &Self::Action);
+    fn perform(&mut self, history: &Vec<Self::Trace>, action: &Self::Action)
+        -> Option<Self::Trace>;
+    fn backtrack(&mut self, history: &Vec<Self::Trace>, trace: Self::Trace);
 
     fn dfs(&mut self) -> Self::Score {
-        let mut history: Vec<Self::Action> = Vec::new();
+        let mut history: Vec<Self::Trace> = Vec::new();
         let mut stack: Vec<Self::Actions> = vec![self.actions()];
         let mut best_score = self.score();
+        let mut counter = 0u64;
         while let Some(actions) = stack.last_mut() {
+            counter += 1;
+            if counter % 10000000 == 0 {
+                println!("{:?}/{:?} ||| {history:?}", self.score(), best_score);
+            }
             if let Some(action) = actions.next() {
-                if self.perform(&history, &action) {
-                    history.push(action);
+                if let Some(trace) = self.perform(&history, &action) {
+                    history.push(trace);
                     stack.push(self.actions());
                 }
             } else {
+                //if self.score() > best_score {
+                //    println!("{best_score:?}");
+                //    println!("{history:?}");
+                //}
                 best_score = best_score.max(self.score());
-                if let Some(prev_action) = history.pop() {
-                    self.backtrack(&history, &prev_action);
+                if let Some(trace) = history.pop() {
+                    self.backtrack(&history, trace);
                 }
                 stack.pop();
             }

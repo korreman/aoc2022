6b9005eace89a55e7a3981884ed6c512ffa7b678
Troels Korreman <troels.korreman@gmail.com>
2022-12-31 00:08:17 +0100

    Solve day 22, part 1

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn main() {
     //state.run_day(19, &solutions::day19::run, "input/day19.txt");
     //state.run_day(20, &solutions::day20::run, "input/day20.txt");
     state.run_day(21, &solutions::day21::run, "input/day21.txt");
+    state.run_day(22, &solutions::day22::run, "input/day22.txt");
 
     state.print();
     println!("{day10part2:?}");

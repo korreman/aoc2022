2bfbd1f48c81a835beb11d19bbaad0315db25100
Troels Korreman <troels.korreman@gmail.com>
2023-03-29 10:20:00 +0200

    Solve day 21, part 2

--- src/solutions/day21.rs
+++ src/solutions/day21.rs
@@ -1,11 +1,7 @@
-use std::collections::VecDeque;
-
 use fxhash::FxHashMap;
 use itertools::Itertools;
 
-use crate::util::graph::{GraphImpl, VecGraph};
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Op {
     Add,
     Sub,
@@ -16,7 +12,7 @@ enum Op {
 #[derive(Debug, Clone, Copy)]
 enum PExpr<'a> {
     Binop(Op, &'a str, &'a str),
-    Num(u64),
+    Num(i64),
 }
 
 impl<'a> PExpr<'a> {
@@ -34,7 +30,7 @@ impl<'a> PExpr<'a> {
 #[derive(Debug, Clone, Copy)]
 enum Expr {
     Binop(Op, usize, usize),
-    Num(u64),
+    Num(i64),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -59,7 +55,7 @@ impl<'a> PMonkey<'a> {
     }
 }
 
-pub fn run(input: &str) -> (u64, u64) {
+pub fn run(input: &str) -> (i64, i64) {
     let monkeys = input.lines().map(PMonkey::parse).collect_vec();
     let indices: FxHashMap<&str, usize> = monkeys
         .iter()
@@ -75,10 +71,11 @@ pub fn run(input: &str) -> (u64, u64) {
         .collect_vec();
 
     // Part 1
-    let mut results = monkeys.iter().map(|_| None).collect_vec();
+    let mut results: Vec<Option<i64>> = monkeys.iter().map(|_| None).collect_vec();
     let mut has_humn = monkeys.iter().map(|_| false).collect_vec();
     has_humn[indices["humn"]] = true;
     let mut stack = vec![indices["root"]];
+    let mut humn_path: Vec<(Op, bool, f64)> = Vec::new();
     while let Some(idx) = stack.pop() {
         if results[idx].is_some() {
             continue;
@@ -93,6 +90,12 @@ pub fn run(input: &str) -> (u64, u64) {
                         Op::Mul => Some(aa * bb),
                         Op::Div => Some(aa / bb),
                     };
+                    if has_humn[a] {
+                        humn_path.push((op, false, bb as f64));
+                    }
+                    if has_humn[b] {
+                        humn_path.push((op, true, aa as f64));
+                    }
                     has_humn[idx] = has_humn[a] || has_humn[b];
                 } else {
                     stack.push(idx);
@@ -106,16 +109,34 @@ pub fn run(input: &str) -> (u64, u64) {
     let res1 = results[indices["root"]].unwrap();
 
     // Part 2
-    let (a, b) = match monkeys[indices["root"]] {
-        Expr::Binop(_, a, b) => (a, b),
-        _ => panic!(),
-    };
-    let human = monkeys[indices["humn"]];
-    println!("{}, {}", has_humn[a], has_humn[b]);
-    // Starting at humn, work your way up the expression DAG and build a series of modifications.
-    // When you reach either a or b, perform all those modifications in reverse on the opposite.
+    // The graph of monkeys is a tree.
+    // We built a path of operations reaching humn in the previous step.
+    // Now we simply retrieve the other side of the equation
+    // and apply the inverse operation for each step in the path.
+    let (_, _, equalee) = humn_path.pop().unwrap();
+    let res2 = humn_path
+        .iter()
+        .rev()
+        .fold(equalee, |acc, (op, flip, num)| match op {
+            Op::Add => acc - num,
+            Op::Sub => {
+                if *flip {
+                    num - acc
+                } else {
+                    acc + num
+                }
+            }
+            Op::Mul => acc / num,
+            Op::Div => {
+                if *flip {
+                    num / acc
+                } else {
+                    acc * num
+                }
+            }
+        });
 
-    (res1, 0)
+    (res1, res2 as i64)
 }
 
 #[cfg(test)]

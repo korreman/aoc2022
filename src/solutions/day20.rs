4bf1cee43405de91a880e9d813fb103a231de18f
Troels Korreman <troels.korreman@gmail.com>
2023-04-14 19:17:01 +0200

    Store rotations in separate array

--- src/solutions/day20.rs
+++ src/solutions/day20.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 pub fn run(input: &str) -> (i64, i64) {
     // parse
     let numbers: Vec<i64> = input
@@ -7,25 +5,21 @@ pub fn run(input: &str) -> (i64, i64) {
         .map(|word| word.parse::<i64>().unwrap())
         .collect();
 
-    let res1 = task(numbers.as_slice(), 1, 1);
-    let res2 = task(numbers.as_slice(), 10, 811589153);
+    let decoded_numbers: Vec<i64> = numbers.iter().map(|n| n * 811_589_153).collect();
+
+    let res1 = task(numbers.as_slice(), 1);
+    let res2 = task(decoded_numbers.as_slice(), 10);
     (res1, res2)
 }
 
-fn task(numbers: &[i64], rounds: usize, multiplier: i64) -> i64 {
+fn task(numbers: &[i64], rounds: usize) -> i64 {
     let len = numbers.len();
-
-    // set up state
-    let mut state: Vec<(usize, i64)> = numbers
-        .iter()
-        .map(|&n| n * multiplier)
-        .enumerate()
-        .collect();
+    let mut state: Vec<u16> = (0..len as u16).collect();
 
     // shuffle
     for _ in 0..rounds {
-        for n in 0..len {
-            let (idx, (_, rotation)) = state.iter().find_position(|(sn, _)| *sn == n).unwrap();
+        for (n, rotation) in numbers.iter().enumerate() {
+            let idx = state.iter().position(|&x| x as usize == n).unwrap();
             let target = (idx as i64 + rotation).rem_euclid((len - 1) as i64) as usize;
             if target > idx {
                 state[idx..=target].rotate_left(1);
@@ -36,12 +30,10 @@ fn task(numbers: &[i64], rounds: usize, multiplier: i64) -> i64 {
     }
 
     // compute result
-    while state[0].1 != 0 {
-        state.rotate_left(1);
-    }
-    let a = state[1000 % len].1;
-    let b = state[2000 % len].1;
-    let c = state[3000 % len].1;
+    let zero_idx = state.iter().position(|&n| numbers[n as usize] == 0).unwrap();
+    let a = numbers[state[(zero_idx + 1000) % len] as usize];
+    let b = numbers[state[(zero_idx + 2000) % len] as usize];
+    let c = numbers[state[(zero_idx + 3000) % len] as usize];
     a + b + c
 }
 

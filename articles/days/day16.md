8143247934368d76635eb76b381d4c51b2c0282f
Troels Korreman <troels.korreman@gmail.com>
2023-11-09 16:27:32 +0100

    day16: WIP article and refactoring

--- articles/days/day16.md
+++ articles/days/day16.md
@@ -1,7 +1,104 @@
-# Day 16
+# Day 16: Proboscidea Volcanium
 
-## Pre-processing
+## Summary
 
-## An efficient DFS
+We've encountered a cave network where each room has a valve in it.
+Each valve has a flow rate N, and a number of tunnels leading to other valves.
+At every minute, we may open a valve or move to an adjacent valve.
+A valve will release N pressure for every minute after it has been opened.
+Our goal is to release as much pressure as possible.
 
-## Branch and bound
+## Part 1
+
+How much pressure can you release in 30 minutes?
+
+### Abstracting the problem
+
+TODO: Is there a tighter classification of this problem?
+
+This exercise can be classified as a
+[combinatorial optimization problem](https://en.wikipedia.org/wiki/Combinatorial_optimization).
+We have a number of turns, a "game state",
+a sequence of available actions based on the state,
+and each action can modify the state and/or increase the score.
+The challenge stems from the vast space of possibilities and the fact
+that the greedy choice isn't always the best one.
+
+We will solve the problem using a state-space search of the decision tree,
+employing various methods to help us:
+
+- Pruning bad branches using branch-and-bound.
+- Predicting the payout of actions up-front.
+
+TODO: Rephrase
+
+- Searching through "meta-actions",
+  sequences of actions that have a meaningful result.
+
+### Pre-processing
+
+If we were to perform a DFS of the decision tree,
+we would be searching through a lot of obviously useless decisions.
+Each valve may lie on the path to another,
+so we cannot mark the visited valves and ignore them.
+In the first place, only 15 of the 61 valves have a non-zero flow rate.
+
+To drastically reduce the search space,
+we create a complete weighted graph of the non-zero valves,
+each edge corresponding to the shortest path between the connected valves.
+
+In this graph, going across a single edge to a node
+corresponds to taking the shortest path to that valve and opening it.
+There is no possibility of aimlessly walking around,
+and we can track which valves have already been opened.
+
+### Branch and bound
+
+The most important factor to improving performance is to reduce the amount of states we search.
+To prune the search space,
+we'll use a [branch and bound](https://en.wikipedia.org/wiki/Branch_and_bound) method.
+It's a method of pruning a state space search,
+and it more or less boils down to:
+
+- Establish a heuristic to search through better prospective paths first.
+- Keep track of a lower bound for the so-far best solution.
+- Skip all branches whose upper bound isn't greater than the current lower bound.
+
+For the __heuristic__, we prioritize opening whichever valve has the greatest flow.
+This could be further specified to
+whichever valve provides the greatest immediate score after travel.
+
+The __lower bound__ is the best score that we have encountered so far.
+
+The __upper bound__ is calculated as an optimistic best final score.
+If we reduced all distances between valves to the shortest of all distances,
+the best score could be obtained by always opening the valve remaining with highest flow.
+
+Pruning our branches this way,
+we only visit __246__ states before we know the absolute best score.
+
+## Part 2
+
+An elephant decides to help out.
+You may each perform an action at every minute.
+Release as much pressure as possible within only 26 minutes.
+
+### Observations
+
+Our goal this time is to find the two non-overlapping solutions (complements)
+that achieve the greatest total score.
+
+### Incorrect attempt
+
+A popular way to solve the second part was to solve for one person first,
+then solve for the elephant using the remaining unvisited valves.
+_This appproach is incorrect._
+There is no guarantee that the best solution and its best complement will yield the highest total.
+
+### Method
+
+1. Find the optimal single path, P.
+2. Find the best complement to P, C.
+3. Traverse all branches that >= C.
+   Store the best score for each set of visited nodes.
+4. Out of all non-zero sets, find the best complementing pair.
--- src/optimized/day16.rs
+++ src/optimized/day16.rs
@@ -4,37 +4,49 @@ use itertools::Itertools;
 use std::cmp::Reverse;
 use std::mem::swap;
 
+// TODO: The choice of abstractions could be better.
+// The branch and bound algorithm could be generalized.
+// One of our instantiations doesn't even track the bound,
+// it's just a depth-first search pruning a static bound.
+// We also compute the upper bound twice.
+//
+// So, maybe we can generalize the depth-first state space search with pruning?
+// And then implement branch and bound as a specific form of pruning.
+// And implement a specific bound.
+//
+// This only makes sense if we can use the same abstractions for day 19.
+
 pub fn run(input: &str) -> (u16, u16) {
     let valves = parse(input);
     let graph = preprocess(valves);
 
     // Part 1
     let mut bound = BestBound::new();
-    BranchState::new(&graph, 30, 0).branch_and_bound(&mut bound);
-    let res1 = bound.best();
+    SearchState::new(&graph, 30, 0).branch_and_bound(&mut bound);
+    let res1 = bound.lower_bound();
 
     // Part 2
     let mut bound1 = BestBound::new();
-    BranchState::new(&graph, 26, 0).branch_and_bound(&mut bound1);
+    SearchState::new(&graph, 26, 0).branch_and_bound(&mut bound1);
 
     let mut bound2 = BestBound::new();
-    BranchState::new(&graph, 26, bound1.best_visited).branch_and_bound(&mut bound2);
+    SearchState::new(&graph, 26, bound1.visited).branch_and_bound(&mut bound2);
 
-    let mut complement_bound = ComplementBound::new(bound2.best, graph.num_valves);
-    BranchState::new(&graph, 26, 0).branch_and_bound(&mut complement_bound);
-    let res2 = complement_bound.best();
+    let mut complement_bound = StaticBound::new(bound2.lower_bound, graph.num_valves);
+    SearchState::new(&graph, 26, 0).branch_and_bound(&mut complement_bound);
+    let res2 = complement_bound.lower_bound();
 
     (res1, res2)
+}
+
+fn parse(input: &str) -> Vec<Valve> {
+    input.lines().map(parse_valve).collect()
 }
 
 struct Valve<'a> {
     flow: u16,
     label: &'a str,
     tunnels: Vec<&'a str>,
-}
-
-fn parse(input: &str) -> Vec<Valve> {
-    input.lines().map(parse_valve).collect_vec()
 }
 
 fn parse_valve(line: &str) -> Valve {
@@ -65,6 +77,8 @@ fn preprocess(valves: Vec<Valve>) -> Graph {
             valves_flow.push(idx);
         }
     }
+    // Rather than track branch priority at a later point,
+    // we sort the branches by flow.
     valves_flow.sort_by_key(|idx| Reverse(valves[*idx].flow));
     valves_flow.push(idxs["AA"]);
 
@@ -123,21 +137,27 @@ fn preprocess(valves: Vec<Valve>) -> Graph {
     }
 }
 
-struct BranchState<'a> {
+struct SearchState<'a> {
+    /// The graph of flow valves.
     graph: &'a Graph,
-    visited: u16,
+    /// Stack of branches representing the current path.
     stack: Vec<usize>,
+    /// A bitset representing which nodes have already been visited.
+    visited: u16,
+    /// The next branch that we are currently considering visiting.
     next: usize,
+    /// The score of the current path.
     score: u16,
+    /// Steps left in the current path.
     steps_left: u16,
 }
 
-impl<'a> BranchState<'a> {
+impl<'a> SearchState<'a> {
     fn new(graph: &'a Graph, steps_left: u16, previsited: u16) -> Self {
         Self {
             graph,
-            visited: previsited,
             stack: vec![graph.valves.len() - 1],
+            visited: previsited,
             next: 0,
             score: 0,
             steps_left,
@@ -151,17 +171,19 @@ impl<'a> BranchState<'a> {
                 let current = *self.stack.last().unwrap();
                 let next_cost = self.graph.dist_matrix[self.next + current * self.graph.num_valves];
                 if next_cost <= self.steps_left && (1 << self.next) & self.visited == 0 {
-                    if !bound.better(&self, &self.graph, next_cost) {
+                    if !bound.better(self, self.graph, next_cost) {
                         self.next += 1;
                         continue;
                     }
-                    self.advance(&self.graph, next_cost);
-                    bound.update(&self);
+                    self.advance(self.graph, next_cost);
+                    // TODO: This should only need to be updated when encountering a leaf,
+                    // can we save some time?
+                    bound.update(self);
                 } else {
                     self.next += 1;
                 }
             } else if self.stack.len() > 1 {
-                self.backtrack(&self.graph);
+                self.backtrack(self.graph);
             } else {
                 break;
             }
@@ -180,7 +202,7 @@ impl<'a> BranchState<'a> {
     #[inline(always)]
     fn backtrack(&mut self, graph: &Graph) {
         let prev = self.stack.pop().unwrap();
-        self.score -= graph.valves[prev as usize] * self.steps_left;
+        self.score -= graph.valves[prev] * self.steps_left;
         let prev_current = *self.stack.last().unwrap();
         self.steps_left += graph.dist_matrix[prev + prev_current * graph.num_valves];
         self.visited ^= 1 << prev;
@@ -189,33 +211,37 @@ impl<'a> BranchState<'a> {
 }
 
 trait Bound {
-    fn update(&mut self, b: &BranchState);
-    fn better(&self, b: &BranchState, g: &Graph, next_cost: u16) -> bool;
-    fn best(&self) -> u16;
+    /// Update the lower bound based on the current state.
+    fn update(&mut self, b: &SearchState);
+    /// Check if the upper bound of the considered branch is better than the lower bound.
+    fn better(&self, b: &SearchState, g: &Graph, next_cost: u16) -> bool;
+    fn lower_bound(&self) -> u16;
 }
 
 struct BestBound {
-    best: u16,
-    best_visited: u16,
+    /// The current lower bound.
+    lower_bound: u16,
+    /// The set of visited nodes for the lower bound.
+    visited: u16,
 }
 
 impl BestBound {
     fn new() -> Self {
-        Self { best: 0, best_visited: 0 }
+        Self { lower_bound: 0, visited: 0 }
     }
 }
 
 impl Bound for BestBound {
     #[inline(always)]
-    fn update(&mut self, b: &BranchState) {
-        if self.best < b.score {
-            self.best = b.score;
-            self.best_visited = b.visited;
+    fn update(&mut self, b: &SearchState) {
+        if self.lower_bound < b.score {
+            self.lower_bound = b.score;
+            self.visited = b.visited;
         }
     }
 
     #[inline(always)]
-    fn better(&self, b: &BranchState, g: &Graph, next_cost: u16) -> bool {
+    fn better(&self, b: &SearchState, g: &Graph, next_cost: u16) -> bool {
         let mut bound = b.score + g.valves[b.next] * (b.steps_left - next_cost);
         let mut steps_left_hypo = b.steps_left - next_cost;
         for valve in 0..g.num_valves {
@@ -225,38 +251,40 @@ impl Bound for BestBound {
             steps_left_hypo -= g.best_dist;
             bound += g.valves[valve] * steps_left_hypo;
         }
-        bound > self.best
+        bound > self.lower_bound
     }
 
     #[inline(always)]
-    fn best(&self) -> u16 {
-        self.best
+    fn lower_bound(&self) -> u16 {
+        self.lower_bound
     }
 }
 
-struct ComplementBound {
+struct StaticBound {
+    /// Tracks the best score for each set of visited valves.
     bests: Vec<u16>,
-    best_complement: u16,
+    /// Lower bound, this is never changed.
+    lower_bound: u16,
 }
 
-impl ComplementBound {
+impl StaticBound {
     fn new(best_complement: u16, num_valves: usize) -> Self {
         Self {
             bests: vec![0; usize::pow(2, num_valves as u32 - 1)],
-            best_complement,
+            lower_bound: best_complement,
         }
     }
 }
 
-impl Bound for ComplementBound {
+impl Bound for StaticBound {
     #[inline(always)]
-    fn update(&mut self, b: &BranchState) {
+    fn update(&mut self, b: &SearchState) {
         let visited = b.visited as usize;
         self.bests[visited] = self.bests[visited].max(b.score);
     }
 
     #[inline(always)]
-    fn better(&self, b: &BranchState, g: &Graph, next_cost: u16) -> bool {
+    fn better(&self, b: &SearchState, g: &Graph, next_cost: u16) -> bool {
         let mut bound = b.score + g.valves[b.next] * (b.steps_left - next_cost);
         let mut steps_left_hypo = b.steps_left - next_cost;
         for valve in 0..g.num_valves {
@@ -266,11 +294,11 @@ impl Bound for ComplementBound {
             steps_left_hypo -= g.best_dist;
             bound += g.valves[valve] * steps_left_hypo;
         }
-        bound > self.best_complement
+        bound > self.lower_bound
     }
 
     #[inline(always)]
-    fn best(&self) -> u16 {
+    fn lower_bound(&self) -> u16 {
         let bests = &self.bests;
         // Collect and sort indices of bests.
         let mut best_paths: Vec<usize> = (0..bests.len()).filter(|&idx| bests[idx] > 0).collect();

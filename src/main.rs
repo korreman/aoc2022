c52894c8d6f935b6be2ad42c8a12eb4b9e3b7936
Troels Korreman <troels.korreman@gmail.com>
2023-11-09 22:01:52 +0100

    Support additional dataset

--- src/main.rs
+++ src/main.rs
@@ -9,47 +9,50 @@ mod solutions;
 pub mod util;
 
 fn main() {
-    let mut state = State::new();
-    state.run_day(01, &optimized::day01::run);
-    state.run_day(02, &solutions::day02::run);
-    state.run_day(03, &solutions::day03::run);
-    state.run_day(04, &solutions::day04::run);
-    state.run_day(05, &solutions::day05::run);
-    state.run_day(06, &optimized::day06::run);
-    state.run_day(07, &solutions::day07::run);
-    state.run_day(08, &solutions::day08::run);
-    state.run_day(09, &solutions::day09::run);
-    state.run_day(10, &solutions::day10::run);
-    state.run_day(11, &optimized::day11::run);
-    state.run_day(12, &solutions::day12::run);
-    state.run_day(13, &optimized::day13::run);
-    state.run_day(14, &optimized::day14::run);
-    state.run_day(15, &optimized::day15::run);
-    state.run_day(16, &optimized::day16::run);
-    state.run_day(17, &solutions::day17::run);
-    state.run_day(18, &solutions::day18::run);
-    state.run_day(19, &optimized::day19::run);
-    state.run_day(20, &solutions::day20::run);
-    state.run_day(21, &solutions::day21::run);
-    state.run_day(22, &solutions::day22::run);
-    state.run_day(23, &optimized::day23::run);
-    state.run_day(24, &optimized::day24::run);
-    state.run_day(25, &solutions::day25::run);
-    state.print();
+    for subfolder in ["a", "b"] {
+        let mut state = State::new(subfolder);
+        state.run_day(01, &optimized::day01::run);
+        state.run_day(02, &solutions::day02::run);
+        state.run_day(03, &solutions::day03::run);
+        state.run_day(04, &solutions::day04::run);
+        state.run_day(05, &solutions::day05::run);
+        state.run_day(06, &optimized::day06::run);
+        state.run_day(07, &solutions::day07::run);
+        state.run_day(08, &solutions::day08::run);
+        state.run_day(09, &solutions::day09::run);
+        state.run_day(10, &solutions::day10::run);
+        state.run_day(11, &optimized::day11::run);
+        state.run_day(12, &solutions::day12::run);
+        state.run_day(13, &optimized::day13::run);
+        state.run_day(14, &optimized::day14::run);
+        state.run_day(15, &optimized::day15::run);
+        state.run_day(16, &optimized::day16::run);
+        state.run_day(17, &solutions::day17::run);
+        state.run_day(18, &solutions::day18::run);
+        state.run_day(19, &optimized::day19::run);
+        state.run_day(20, &solutions::day20::run);
+        state.run_day(21, &solutions::day21::run);
+        state.run_day(22, &solutions::day22::run);
+        state.run_day(23, &optimized::day23::run);
+        state.run_day(24, &optimized::day24::run);
+        state.run_day(25, &solutions::day25::run);
+        state.print();
+    }
 }
 
-struct State {
+struct State<'a> {
+    subfolder: &'a str,
     table: Table,
     total: Duration,
     large_answers: Vec<String>,
 }
 
-impl State {
-    fn new() -> Self {
+impl<'a> State<'a> {
+    fn new(subfolder: &'a str) -> Self {
         let mut table = Table::new();
         table.load_preset(UTF8_FULL_CONDENSED);
         table.set_header(vec!["Day", "Part 1", "Part 2", "Time"]);
-        Self { table, total: Duration::ZERO, large_answers: Vec::new() }
+        Self { subfolder, table, total: Duration::ZERO, large_answers: Vec::new() }
     }
 
     fn run_day<A1: Display, A2: Display>(
@@ -58,7 +61,7 @@ impl State {
         task: &dyn Fn(&str) -> (A1, A2),
     ) -> (A1, A2) {
         // Read input
-        let input_path = format!("data/a/inputs/day{day:02}.txt");
+        let input_path = format!("data/{}/inputs/day{day:02}.txt", self.subfolder);
         let input = std::fs::read_to_string(&input_path)
             .expect(format!("missing input: {input_path}").as_str());
 
